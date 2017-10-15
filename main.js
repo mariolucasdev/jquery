e85940bc2f68e4ad074e7a27885e3c0b1dc0f5c9
Mário Lucas <maryosk8bkt@gmail.com>
2017-10-14 23:33:01 -0300

    refactory js code

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
+const hideObj = (obj) => $(obj).hide();
+const showObj = (obj) => $(obj).fadeIn();
+
 $(document).ready(function(){
-    $(".col-md-4").hide();
+    hideObj(".col-md-4");
 
-    $(".btn").click(function(){
-        let idButton = $(this).attr('id');
-        $(".col-md-4").hide();
-        $("#div-"+idButton).fadeIn();
+    $("#showAll").click( () => showObj(".col-md-4"));
+    $("#hideAll").click( () => showObj(".col-md-4"));
+
+    $(".btn").click( function() {
+        let obj = "#div-" + $(this).attr('id');
+        hideObj('.col-md-4');
+        showObj(obj);
     });
 
-    $("#showAll").click(function(){
-        $(".col-md-4").fadeIn();
-    });
-    $("#hideAll").click(function(){
-        $(".col-md-4").fadeOut();
-    });
 });
